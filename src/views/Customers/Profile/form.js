d370450bd8da98ea7742f60d105ca6623059c784
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-09-29 15:23:35 +1300

    stepper

--- src/views/Customers/Profile/form.js
+++ src/views/Customers/Profile/form.js
@@ -4,22 +4,51 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import DatePicker from '@mui/lab/DatePicker';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import Autocomplete from '@mui/material/Autocomplete';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import CircularProgress from "@mui/material/CircularProgress";
 import Grid from '@mui/material/Grid';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Stepper from '@mui/material/Stepper';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { format, parseISO } from 'date-fns';
 import * as PropTypes from 'prop-types';
 import countryList from 'react-select-country-list';
 
 import { useApi } from '../context';
 
+const steps = ['Basic Info', 'Address'];
+
 export const Form = ({ onAction }) => {
     const [state, { update, create }] = useApi();
     const [person, setPerson] = useState();
     const [errorMsg, setErrorMsg] = useState(false);
     const [submitting, setSubmitting] = useState();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [skipped, setSkipped] = React.useState(new Set());
+
+    const isStepSkipped = (step) => {
+        return skipped.has(step);
+    };
+
+    const handleNext = () => {
+        let newSkipped = skipped;
+        if (isStepSkipped(activeStep)) {
+            newSkipped = new Set(newSkipped.values());
+            newSkipped.delete(activeStep);
+        }
+
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped(newSkipped);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
 
     const validEmail = () => {
         let isValid = true;
@@ -82,108 +111,143 @@ export const Form = ({ onAction }) => {
     return (
         <>
             {person &&
-                <>
-                    <Grid container spacing={1}>
-                        <Grid item xs={6}>
-                            <TextInput id="firstName" label="First Name"
-                                size="small" variant="outlined" fullWidth
-                                value={person && person.firstName}
-                                onChange={handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={6} >
-                            <TextInput id="lastName" label="Last Name"
-                                size="small" variant="outlined" fullWidth
-                                value={person && person.lastName}
-                                onChange={handleChange}
-                            />
-
-                        </Grid>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="flex-start"
+                >
+                    <Grid item xs={3}>
+                        <Stepper orientation="vertical" activeStep={activeStep}>
+                            {steps.map((label, index) => {
+                                const stepProps = {};
+                                const labelProps = {};
+                                if (isStepSkipped(index)) {
+                                    stepProps.completed = false;
+                                }
+                                return (
+                                    <Step key={label} {...stepProps}>
+                                        <StepLabel {...labelProps}>{label}</StepLabel>
+                                    </Step>
+                                );
+                            })}
+                        </Stepper>
                     </Grid>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        {/* <DatePicker
-                            // views={person ? '' : ['day', 'month', 'year']}
-                            id="birthDate"
-                            label="Date of Birth"
-                            onChange={e => handleChange({ target: { id: "birthDate", value: person ? format(e, 'yyyy-MM-dd') : e } })}
-                            value={person ? parseISO(person.birthDate) : person.birthDate}
+                    <Grid item xs={9} >
+                        {activeStep === 0 &&
+                            <>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={6}>
+                                        <TextInput id="firstName" label="First Name"
+                                            size="small" variant="outlined" fullWidth
+                                            value={person && person.firstName}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={6} >
+                                        <TextInput id="lastName" label="Last Name"
+                                            size="small" variant="outlined" fullWidth
+                                            value={person && person.lastName}
+                                            onChange={handleChange}
+                                        />
 
-                            renderInput={(params) =>
-                                <TextInput id="birthDate" size="small" variant="outlined" fullWidth {...params} />
-                            }
-                        /> */}
-                        <DatePicker
-                            id="birthDate"
-                            label="Date of Birth"
-                            onChange={e => handleChange({ target: { id: "birthDate", value: format(e, 'yyyy-MM-dd') } })}
-                            value={person && parseISO(person.birthDate)}
-                            renderInput={(params) =>
-                                <TextInput id="birthDate" size="small" variant="outlined" fullWidth {...params} />
-                            }
-                        />
-                    </LocalizationProvider>
-                    <TextInput id="email" label="Email Address"
-                        size="small" variant="outlined" fullWidth
-                        value={person && person.email}
-                        onChange={handleChange}
-                        error={Boolean(errorMsg)}
-                        helperText={errorMsg}
-                    />
-                    <TextInput id="phone" label="Phone Number"
-                        size="small" variant="outlined" fullWidth type="number"
-                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                        value={person && person.phone}
-                        onChange={handleChange}
-                    />
-                    <TextInput id="addressLine1" label="Address"
-                        size="small" variant="outlined" fullWidth
-                        value={person && person.addressLine1}
-                        onChange={handleChange}
-                    />
-                    <TextInput id="addressLine2" label="Secondary Address"
-                        size="small" variant="outlined" fullWidth
-                        value={person && person.addressLine2}
-                        onChange={handleChange}
-                    />
-                    <Grid container spacing={1}>
-                        <Grid item xs={6}>
-                            <TextInput id="suburb" label="Suburb"
-                                fullWidth
-                                size="small" variant="outlined"
-                                value={person && person.suburb}
-                                onChange={handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={6} >
-                            <TextInput id="city" label="City"
-                                size="small" variant="outlined" fullWidth
-                                value={person && person.city}
-                                onChange={handleChange}
-                            />
+                                    </Grid>
+                                </Grid>
+                                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <DatePicker
+                                        id="birthDate"
+                                        label="Date of Birth"
+                                        onChange={e => handleChange({ target: { id: "birthDate", value: format(e, 'yyyy-MM-dd') } })}
+                                        value={person && parseISO(person.birthDate)}
+                                        renderInput={(params) =>
+                                            <TextInput id="birthDate" size="small" variant="outlined" fullWidth {...params} />
+                                        }
+                                    />
+                                </LocalizationProvider>
+                                <TextInput id="email" label="Email Address"
+                                    size="small" variant="outlined" fullWidth
+                                    value={person && person.email}
+                                    onChange={handleChange}
+                                    error={Boolean(errorMsg)}
+                                    helperText={errorMsg}
+                                />
+                                <TextInput id="phone" label="Phone Number"
+                                    size="small" variant="outlined" fullWidth type="number"
+                                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                                    value={person && person.phone}
+                                    onChange={handleChange}
+                                />
+                            </>
+                        }
 
-                        </Grid>
+                        <React.Fragment>
+                            {activeStep === 1 &&
+                                <>
+                                    <TextInput id="addressLine1" label="Address"
+                                        size="small" variant="outlined" fullWidth
+                                        value={person && person.addressLine1}
+                                        onChange={handleChange}
+                                    />
+                                    <TextInput id="addressLine2" label="Secondary Address"
+                                        size="small" variant="outlined" fullWidth
+                                        value={person && person.addressLine2}
+                                        onChange={handleChange}
+                                    />
+                                    <Grid container spacing={1}>
+                                        <Grid item xs={6}>
+                                            <TextInput id="suburb" label="Suburb"
+                                                fullWidth
+                                                size="small" variant="outlined"
+                                                value={person && person.suburb}
+                                                onChange={handleChange}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={6} >
+                                            <TextInput id="city" label="City"
+                                                size="small" variant="outlined" fullWidth
+                                                value={person && person.city}
+                                                onChange={handleChange}
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                    <Autocomplete
+                                        id="country"
+                                        onChange={e => handleChange({ target: { id: "country", value: e } })}
+                                        getOptionLabel={options.label}
+                                        // value={person ? person.country : options.label}
+                                        options={options.map((option) => option.label)}
+                                        // defaultValue={person && person.country}
+                                        isOptionEqualToValue={(option, e) => option.code === e}
+                                        renderInput={(params) => <TextInput size="small" id="country" variant="outlined" label="Country" fullWidth {...params} />}
+                                    />
+                                </>
+                            }
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Button
+                                    color="inherit"
+                                    disabled={activeStep === 0}
+                                    onClick={handleBack}
+                                    sx={{ mr: 1 }}
+                                >
+                                    Back
+                                </Button>
+                                <Box sx={{ flex: '1 1 auto' }} />
+
+                                {activeStep === steps.length - 1 ?
+                                    <Button
+                                        sx={{ marginTop: 2 }}
+                                        variant="contained"
+                                        onClick={handleSave}
+                                        disabled={!formValid()}
+                                    >
+                                        {submitting ? <CircularProgress size={24} /> : person.id ? 'Update Person' : 'Create Person'}
+                                    </Button> : <Button disabled={!formValid()} onClick={handleNext}>
+                                        Next
+                                    </Button>}
+                            </Box>
+                        </React.Fragment>
                     </Grid>
-                    <Autocomplete
-                        id="country"
-                        onChange={e => handleChange({ target: { id: "country", value: e } })}
-                        getOptionLabel={options.label}
-                        // value={person ? person.country : options.label}
-
-                        options={options.map((option) => option.label)}
-                        // defaultValue={person && person.country}
-                        isOptionEqualToValue={(option, e) => option.code === e}
-                        renderInput={(params) => <TextInput size="small" id="country" variant="outlined" label="Country" fullWidth {...params} />}
-                    />
-                    <Button
-                        sx={{ marginTop: 2 }}
-                        variant="contained"
-                        onClick={handleSave}
-                        disabled={!formValid()}
-                    >
-                        {submitting ? <CircularProgress size={24} /> : person.id ? 'Update Person' : 'Create Person'}
-                    </Button>
-
-                </>
+                </Grid>
             }
         </>
     )
@@ -196,4 +260,3 @@ const TextInput = styled(TextField)(({ theme }) => ({
 Form.propTypes = {
     onAction: PropTypes.func,
 };
-
