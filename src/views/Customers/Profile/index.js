a54bceb57a065bbaa83eea9b5c587b9b7581e2f7
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-09-30 15:43:44 +1300

    profile ui update

--- src/views/Customers/Profile/index.js
+++ src/views/Customers/Profile/index.js
@@ -4,11 +4,16 @@ import CloseIcon from '@mui/icons-material/Close';
 import CreateIcon from '@mui/icons-material/Create';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
 import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { format, parseISO } from 'date-fns';
+import Moment from 'react-moment';
 import { useParams } from "react-router-dom";
 
 import { Trail } from '../../../components/Trail';
@@ -48,6 +53,7 @@ const Customer = () => {
         window.location.reload(false);
     }
 
+
     return (
         <>
             {person &&
@@ -75,35 +81,43 @@ const Customer = () => {
                                 direction="column"
                                 spacing={2}
                             >
-                                <Card sx={{ p: 1 }}>
-                                    <Typography variant="h2">
-                                        {person.firstName} {person.lastName}
-                                    </Typography>
-                                    <Typography >
-                                        {person.birthDate !== '' && format(parseISO(person.birthDate), 'dd MMMM yyyy')}
-                                    </Typography>
-                                    <Typography >
-                                        {person.email}
-                                    </Typography>
-                                    <Typography >
-                                        {person.phone}
-                                    </Typography>
+                                <Card sx={{ mt: 1, p: 1 }}>
+                                    <CardHeader title={`${person.firstName} ${person.lastName}`} />
+                                    <ListItem>
+                                        <ListItemText>Birth Date</ListItemText>{person.birthDate !== '' && format(parseISO(person.birthDate), 'dd MMMM yyyy')}
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem>
+                                        <ListItemText>Email</ListItemText>{person.email}
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem>
+                                        <ListItemText>Phone Number</ListItemText>{person.phone}
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem>
+                                        <ListItemText>Account Created</ListItemText><Moment fromNow interval={30000} date={person.date} />
+                                    </ListItem>
                                 </Card>
 
-                                <Card sx={{ p: 1 }}>
-                                    <Typography >
-                                        {person.addressLine1}
-                                    </Typography>
-                                    <Typography >
-                                        {person.addressLine2}
-                                    </Typography>
-                                    <Typography >
-                                        {person.suburb}, {person.city}
-                                    </Typography>
-                                    <Typography >
-                                        {person.region} ,  {person.country}
-                                    </Typography>
-                                </Card>
+                                {person.address &&
+                                    <Card sx={{ p: 1 }}>
+                                        <CardHeader title="Address" />
+                                        <ListItem>
+                                            <Typography >
+                                                {person.addressLine1}
+                                                <br />
+                                                {person.addressLine2}
+                                                <br />
+                                                {person.suburb && ""} {person.city}
+                                                <br />
+                                                {person.region}
+                                                <br />
+                                                {person.country}
+                                            </Typography>
+                                        </ListItem>
+                                    </Card>
+                                }
                             </Stack>
                         ) : (
                             <Form onAction={submit} />
