7e3b7a29a38d98e48a39de9bf1d9e3e629d0745e
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-10-21 08:04:01 +1300

    fix likes

--- src/components/DisplayCard/index.js
+++ src/components/DisplayCard/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { useAuth0 } from "@auth0/auth0-react";
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ShareIcon from '@mui/icons-material/Share';
 import { CardActionArea, CardHeader } from '@mui/material';
@@ -20,8 +22,6 @@ import Popover from '@mui/material/Popover';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import * as PropTypes from 'prop-types';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import ActionLink from '../ActionLink';
 
@@ -41,53 +41,52 @@ export default DisplayCard;
 
 const BigDisplayCard = props => {
     const { title, subtitle, description, price, string, to, image } = props;
-    const [view, setView] = useState('false');
+    const [view, setView] = useState(false);
 
     return (
         <Card sx={{ m: 1 }}>
-            <CardActionArea component={ActionLink} to={`/products/${string}` || to} >
-                <CardMedia
-                    component="img"
-                    height="200"
-                    image={image}
-                    alt={`${image} text`}
-                />
-                <CardHeader
-                    title={<Typography variant="h5">
-                        {title}
-                    </Typography>}
-                    subheader={subtitle}
-                />
-                <ListItem>
-                    <Typography noWrap variant="body2" color="text.secondary">
-                        {description}
+            {/* <CardActionArea component={ActionLink} to={`/products/${string}` || to} > */}
+            <CardMedia
+                component="img"
+                height="200"
+                image={image}
+                alt={`${image} text`}
+            />
+            <CardHeader
+                title={<Typography variant="h5">
+                    {title}
+                </Typography>}
+                subheader={subtitle}
+            />
+            <ListItem>
+                <Typography noWrap variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </ListItem>
+            <ListItem>
+                <Stack
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="flex-start"
+                    spacing={1}
+                >
+                    <Typography color="text.secondary" variant="caption">
+                        Price
                     </Typography>
-                </ListItem>
-                <ListItem>
-                    <Stack
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="flex-start"
-                        spacing={1}
-                    >
-                        <Typography color="text.secondary" variant="caption">
-                            Price
-                        </Typography>
-                        <Typography variant="body1">
-                            {`$${price}`}
-                        </Typography>
-                    
-                    </Stack>
-                </ListItem>
-            </CardActionArea>
-            {view === ('false') &&
-            <IconButton onClick={() => setView('true')} sx={{ marginLeft: 30 }}>
-                <FavoriteBorderIcon />
-            </IconButton>}
-            {view === ('true') &&  
-            <IconButton onClick={() => setView('false')} sx={{ marginLeft: 30 }}>
-                <FavoriteIcon />
-            </IconButton> }
+                    <Typography variant="body1">
+                        {`$${price}`}
+                    </Typography>
+                    <IconButton onClick={() => view === false ? setView(true) : setView(false)} sx={{ marginLeft: 30 }}>
+                        {view === false ?
+                            <FavoriteBorderIcon />
+                            :
+                            <FavoriteIcon />
+                        }
+                    </IconButton>
+                </Stack>
+
+            </ListItem>
+            {/* </CardActionArea> */}
 
         </Card>
     )
