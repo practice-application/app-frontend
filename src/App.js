7df7ee18da4ee15234eea45f9fe7dfff091ef675
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-11-10 09:45:13 +1300

    push

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { Layout } from './components/Layout';
 import { WaitSkeleton } from './components/WaitSkeleton';
 import { custom } from './theme';
 
-
 const Profile = lazy(() => import('./views/Customers/Profile'));
 const Create = lazy(() => import('./views/Customers/Profile/Create'));
 const CustomerList = lazy(() => import('./views/Customers/CustomerList'));
@@ -17,8 +16,6 @@ const CustomerList = lazy(() => import('./views/Customers/CustomerList'));
 const Listing = lazy(() => import('./views/Products/Listing'));
 const ProductPage = lazy(() => import('./views/Products/ProductPage'));
 const CreateProduct = lazy(() => import('./views/Products/Listing/CreateProduct'));
-
-
 
 let theme = createTheme(custom);
 theme = responsiveFontSizes(theme)
